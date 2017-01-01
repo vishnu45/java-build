46390b69b8072f6f5979f662b89b36ac4151fff4
vishnu45 <vishnu4v5@gmail.com>
2017-01-01 00:50:13 -0500

    First commit

---
+++ src/main/java/com/workshop/vish/puzzles/App.java
@@ -0,0 +1,13 @@
+package com.workshop.vish.puzzles;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
